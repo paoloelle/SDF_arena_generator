45ef295ce744147d6bbbdb4192fa0a72c0b499d1
Lukas Sonnenmoser <lukas.sonnenmoser@uni.kn>
2023-12-15 16:27:08 +0100

    fixed collision and pose

--- create_ground.py
+++ create_ground.py
@@ -16,7 +16,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
 
     # cache position
     CACHE_POSE_X = NEST_POSE_X
-    CACHE_POSE_Y = NEST_SIZE_Y / 2 + CACHE_SIZE_Y / 2
+    CACHE_POSE_Y = (NEST_POSE_Y + NEST_SIZE_Y / 2) + CACHE_SIZE_Y / 2
     CACHE_POSE_Z = NEST_POSE_Z
 
     # cache size
@@ -27,7 +27,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
 
     # position collision lower area
     COLLISION_LOWER_AREA_POSE_X = NEST_POSE_X
-    COLLISION_LOWER_AREA_POSE_Y = (NEST_POSE_Y + CACHE_POSE_Y) / 2
+    COLLISION_LOWER_AREA_POSE_Y = (NEST_POSE_Y*NEST_SIZE_Y + CACHE_POSE_Y*CACHE_SIZE_Y) / (NEST_SIZE_Y+CACHE_SIZE_Y)
     COLLISION_UPPER_AREA_POSE_Z = NEST_POSE_Z
 
     # size collision lower area
@@ -49,7 +49,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
 
     # joint cache-slope position relative to link lower area
     JOINT_CACHE_SLOPE_POSE_X = 0
-    JOINT_CACHE_SLOPE_POSE_Y = NEST_SIZE_Y/2 + CACHE_SIZE_Y
+    JOINT_CACHE_SLOPE_POSE_Y = CACHE_POSE_Y + CACHE_SIZE_Y/2
     JOINT_CACHE_SLOPE_POSE_Z = 0
 
     # joint slope-source position relative to link slope
@@ -98,7 +98,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
     # visual cache area
     visual_cache = et.SubElement(link_lower_area, 'visual', name='cache_area')
 
-    pose_cache = et.SubElement(visual_nest, 'pose')
+    pose_cache = et.SubElement(visual_cache, 'pose')
     pose_cache.text = f'{CACHE_POSE_X} {CACHE_POSE_Y} {CACHE_POSE_Z} 0 0 0'
 
     geometry_cache = et.SubElement(visual_cache, 'geometry')
@@ -115,7 +115,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
     pose_collision_lower_area = et.SubElement(collision_lower_area, 'pose')
     pose_collision_lower_area.text = f'{COLLISION_LOWER_AREA_POSE_X} {COLLISION_LOWER_AREA_POSE_Y} {COLLISION_UPPER_AREA_POSE_Z} 0 0 0'
 
-    geometry_collision_lower_area = et.SubElement(pose_collision_lower_area, 'geometry')
+    geometry_collision_lower_area = et.SubElement(collision_lower_area, 'geometry')
     box_collision_lower_area = et.SubElement(geometry_collision_lower_area, 'box')
     size_box_collision_lower_area = et.SubElement(box_collision_lower_area, 'size')
     size_box_collision_lower_area.text = f'{COLLISION_LOWER_AREA_SIZE_X} {COLLISION_LOWER_AREA_SIZE_Y} {COLLISION_LOWER_AREA_SIZE_Z}'
@@ -180,7 +180,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
     child_joint_slope_source.text = 'link_slope'
 
     pose_joint_slope_source = et.SubElement(joint_cache_slope, 'pose', relative_to='link_lower_area')
-    pose_joint_slope_source.text = f'{JOINT_CACHE_SLOPE_POSE_X} {JOINT_CACHE_SLOPE_POSE_Y} {JOINT_CACHE_SLOPE_POSE_Z} 0 {SLOPE_ANGLE} 0'
+    pose_joint_slope_source.text = f'{JOINT_CACHE_SLOPE_POSE_X} {JOINT_CACHE_SLOPE_POSE_Y} {JOINT_CACHE_SLOPE_POSE_Z} {SLOPE_ANGLE} 0  0'
 
     axis_joint_cache_slope = et.SubElement(joint_cache_slope, 'axis')
     xyz_axis_joint_cache_slope = et.SubElement(axis_joint_cache_slope, 'xyz')
@@ -197,7 +197,7 @@ def create_ground(NEST_SIZE_X, NEST_SIZE_Y, CACHE_SIZE_Y, SLOPE_SIZE_Y, SLOPE_AN
     child_joint_slope_source.text = 'link_source_area'
 
     pose_joint_slope_source = et.SubElement(joint_slope_source, 'pose', relative_to='link_slope')
-    pose_joint_slope_source.text = f'{JOINT_SLOPE_SOURCE_POSE_X} {JOINT_SLOPE_SOURCE_POSE_Y} {JOINT_SLOPE_SOURCE_POSE_Z} 0 {-SLOPE_ANGLE} 0'
+    pose_joint_slope_source.text = f'{JOINT_SLOPE_SOURCE_POSE_X} {JOINT_SLOPE_SOURCE_POSE_Y} {JOINT_SLOPE_SOURCE_POSE_Z} {-SLOPE_ANGLE} 0 0'
 
     axis_joint_slope_source = et.SubElement(joint_slope_source, 'axis')
     xyz_axis_joint_slope_source = et.SubElement(axis_joint_slope_source, 'xyz')
